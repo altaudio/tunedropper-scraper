0d843f20a4dc06b5039db4655d4aec89539e3a34
Alex Smith <altaudiolabs@gmail.com>
2020-03-05 18:20:14 +0000

    Finally got access token and refresh token

--- src/server/index.js
+++ src/server/index.js
@@ -1,17 +1,22 @@
 import express from 'express';
+import superagent from 'superagent';
 import dotenv from 'dotenv';
+import btoa from 'btoa';
 
 dotenv.config();
 
 const app = express();
 
+const { SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET, SECRET_KEY, REDIRECT_URI } = process.env;
+
+const redirectURI = encodeURIComponent(`${REDIRECT_URI}/authorized`);
+
 app.get('/authorize', (request, response) => {
-  if (request.query.secretKey !== process.env.SECRET_KEY) {
+  if (request.query.secretKey !== SECRET_KEY) {
     console.log('Error: Incorrect secret key');
     return response.redirect('https://google.com');
   }
 
-  const redirectURI = encodeURIComponent('https://0a15ac8e.ngrok.io/authorized');
   const scopes = encodeURIComponent(
     'playlist-modify-public playlist-modify-private playlist-read-private'
   );
@@ -20,7 +25,7 @@ app.get('/authorize', (request, response) => {
     'https://accounts.spotify.com/authorize' +
       '?response_type=code' +
       '&client_id=' +
-      process.env.SPOTIFY_CLIENT_ID +
+      SPOTIFY_CLIENT_ID +
       '&scope=' +
       scopes +
       '&redirect_uri=' +
@@ -28,16 +33,31 @@ app.get('/authorize', (request, response) => {
   );
 });
 
-app.get('/authorized', (request, response) => {
+app.get('/authorized', async (request, response) => {
   const error = request.query.error;
   if (error) {
-    console.log(`Authorization failed: ${error}`)
+    console.log(`Authorization failed: ${error}`);
   }
 
-  const authorizationCode = request.query.code
-  response.sendStatus(200)
-})
+  const authorizationCode = request.query.code;
+  response.sendStatus(200);
 
-const port = 9090;
+  let accessTokensResponse;
+
+  try {
+    accessTokensResponse = await superagent
+      .post('https://accounts.spotify.com/api/token')
+      .set('Authorization', `Basic ${btoa(`${SPOTIFY_CLIENT_ID}:${SPOTIFY_CLIENT_SECRET}`)}`)
+      .send('grant_type=authorization_code')
+      .send(`code=${authorizationCode}`)
+      .send(`redirect_uri=${redirectURI}`);
+  } catch (error) {
+    console.log(`Error gettings access tokens: ${error}`);
+  }
+
+  console.log(accessTokensResponse);
+});
+
+const port = 8080;
 console.log(`Listening on port ${port}`);
 app.listen(port);
