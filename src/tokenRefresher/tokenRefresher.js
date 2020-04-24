d934290b75f404194bc1c1850f2249cf876bfbde
Alex Smith <altaudiolabs@gmail.com>
2020-04-24 17:49:08 +0100

    Refresh token every 10 mins

--- src/tokenRefresher/tokenRefresher.js
+++ src/tokenRefresher/tokenRefresher.js
@@ -2,7 +2,7 @@ import { callEvery } from '../common/callEvery.js';
 import { refreshAccessToken } from '../spotify/api/tokens/refreshAccessToken.js';
 import { write } from '../database/write.js';
 
-const REFRESH_INTERVAL = 1800000;
+const REFRESH_INTERVAL = 600000;
 
 callEvery(
   async () => {
