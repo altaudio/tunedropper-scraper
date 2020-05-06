be534657306a4619f50e10dfc1b506e7ca88136c
Alex Smith <altaudiolabs@gmail.com>
2020-05-06 08:18:05 +0100

    Wraps callEvery callback in try catch

--- src/common/callEvery.js
+++ src/common/callEvery.js
@@ -1,6 +1,11 @@
 export const callEvery = async (callback, interval, { initialCall } = { initialCall: true }) => {
   if (initialCall) {
-    await callback();
+    try {
+      await callback();
+    } catch (error) {
+      console.log('Something went wrong generally');
+      console.log(error);
+    }
   }
   setTimeout(() => callEvery(callback, interval), interval);
 };
